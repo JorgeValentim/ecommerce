959c9d867522fdb396967a0af8c5545fe33529e6
Jorge Valentim <jorgevalentimp@outlook.com>
2020-06-03 09:35:39 -0300

    Esquexeu a Senha - Site

--- index.php
+++ index.php
@@ -624,6 +624,61 @@ $app->post("/register", function(){
 	exit;
 });
 
+$app->get("/forgot", function() {
+	$page = new Page();
+
+	$page->setTpl("forgot");
+});
+
+$app->post("/forgot", function(){
+	
+	$user = User::getForgot($_POST["email"]);
+
+	header("Location: /orgot/sent");
+	exit;
+});
+
+$app->get("/forgot/sent", function(){
+	
+	$page = new Page();
+
+	$page->setTpl("forgot-sent");
+
+});
+
+$app->get("/forgot/reset", function(){
+
+	$user = User::validForgotDecrypt($_GET["code"]);
+
+	$page = new Page();
+
+	$page->setTpl("forgot-reset", array(
+		"name"=>$user["desperson"],
+		"code"=>$_GET["code"]
+	));
+
+});
+
+$app->post("/forgot/reset", function(){
+	$forgot = User::validForgotDecrypt($_POS["code"]);
+
+	User::setForgot($forgot["idrecovery"]);
+
+	$user = new User();
+
+	$user->get((int)$forgot["iduser"]);
+
+	$password = password_hash($_POST["password"], PASSWORD_DEFAULT, [
+		"cost"=>12
+	]);
+
+	$user->setPassword($password);
+
+	$page = new Page();
+
+	$page->setTpl("forgot-reset-success");
+});
+
 $app->run();
 
  ?>
